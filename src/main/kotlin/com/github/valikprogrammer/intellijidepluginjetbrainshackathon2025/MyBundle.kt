f4750e3e4d4ababa4a0ccb42d6d18a8fb3afcee3
GitHub Action <action@github.com>
2025-09-15 06:55:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/valikprogrammer/intellijidepluginjetbrainshackathon2025/MyBundle.kt
+++ src/main/kotlin/com/github/valikprogrammer/intellijidepluginjetbrainshackathon2025/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.valikprogrammer.intellijidepluginjetbrainshackathon2025
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
